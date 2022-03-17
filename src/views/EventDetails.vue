51992bb80f6a51ff9995dc87bec372ff218eee3a
Erikson Chen <botan3951@gmail.com>
2022-03-17 14:58:55 +0800

    first commit

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,32 @@
+import EventService from '@/services/EventService.js';
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+export default {
+  // props: ['id'],
+  data() {
+    return {
+      event: null,
+    }
+  },
+  created() {
+    // console.log(this.$route.params.id)
+    EventService.getEvent(this.$route.params.id)
+      // EventService.getEvent(this.id)
+      .then((res) => {
+        this.event = res.data
+        console.log(this.event.title)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
+}
+</script>
---
+++ src/views/EventList.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Event For Good</h1>
+  <div class="event">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((res) => {
+        console.log(res.data)
+        this.events = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
+}
+</script>
+
+<style scoped>
+.event {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
